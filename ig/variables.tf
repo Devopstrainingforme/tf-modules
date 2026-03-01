a7168109bcc6adb4f62a7a0cc26b0e7d33f04fe8
Ravikumar <Ravikumar@ravi>
2026-03-02 01:17:50 +0530

    creating vpc resources

--- ig/variables.tf
+++ ig/variables.tf
@@ -1,28 +1,3 @@
-variable "vpc_cidr" {
-    default = "10.0.0.0/16"
-}
-
 variable "project_name" {
   type = string
-}
-
-variable "vpc_id" {
-  type = string
-}
-
-variable "public_subnet_cidrs" {
-  type = list(string)
-}
-
-variable "private_subnet_cidrs" {
-  type = list(string)
-}
-
-variable "azs" {
-  type = list(string)
-}
-
-variable "public_subnet_id" {
-  description = "Public subnet for NAT Gateway"
-  type        = string
 }
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -5,24 +5,3 @@ variable "vpc_cidr" {
 variable "project_name" {
   type = string
 }
-
-variable "vpc_id" {
-  type = string
-}
-
-variable "public_subnet_cidrs" {
-  type = list(string)
-}
-
-variable "private_subnet_cidrs" {
-  type = list(string)
-}
-
-variable "azs" {
-  type = list(string)
-}
-
-variable "public_subnet_id" {
-  description = "Public subnet for NAT Gateway"
-  type        = string
-}
