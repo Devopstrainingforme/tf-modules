13f0cdca164a91e0d89a314b410e4422c0f6fe80
Ravikumar <Ravikumar@ravi>
2026-03-20 02:37:32 +0530

    creating alb

--- providers.tf
+++ providers.tf
@@ -25,15 +25,13 @@ data "aws_eks_cluster_auth" "eks" {
   name = module.eks.cluster_name
 }
 
-provider "kubernetes" {
-  alias                  = "eks"   
+provider "kubernetes" {  
   host                   = data.aws_eks_cluster.eks.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.eks.token
 }
 
 provider "helm" {
-  alias = "eks"  
   kubernetes {
     host                   = data.aws_eks_cluster.eks.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
