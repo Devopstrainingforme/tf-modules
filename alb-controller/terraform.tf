c9994c9604e31e7455d4369d9da0150b574bb2d8
Ravikumar <Ravikumar@ravi>
2026-03-20 02:27:28 +0530

    creating alb

---
+++ alb-controller/terraform.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+
+    helm = {
+      source = "hashicorp/helm"
+    }
+
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
